06d0640b590f950bb3118eea7aa3d589c6f9d4fb
Trenton Kress <trenton.kress@gmail.com>
2022-02-24 12:45:35 -0800

    Please

--- src/v1/routes/api.js
+++ src/v1/routes/api.js
@@ -3,14 +3,24 @@ const router = require('express-promise-router')();
 const logger = log4js.getLogger(`GH-WEBHOOKER:${require('path').parse(module.filename).name}`);
 const WebhookManager = require('discord-webhook-manager');
 
+const webhooks = {};
+
+Object.keys(config.webhookMapping).forEach(key => {
+    webhooks[key] = new WebhookManager(config.webhookMapping[key].id, config.webhookMapping[key].token)
+});
+
 router.post('/ghwh', async (req, res, next) => {
     let data = req.body;
     let webhookData = config.webhookMapping[data.repository.name];
     if (!data.commits || !webhookData || data.ref != 'refs/heads/master') {
-        res.status(500).end({ commits: data.commits, webhookData, ref: data.ref });
+        res.status(500).end({ error: "wrong ref or data", commits: data.commits, webhookData, ref: data.ref });
         return;
     }
-    let webhook = new WebhookManager(webhookData.id, webhookData.token);
+    let webhook = webhooks[data.repository.name];
+    if (!webhook) {
+        res.status(500).end({ error: "webhook not found", commits: data.commits, webhookData, ref: data.ref });
+        return;
+    }
     for (i in data.commits) {
         let commit = data.commits[i];
         if (commit.message.includes('Merge'))
