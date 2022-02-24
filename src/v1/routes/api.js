98358734142b5b57f70ebd9dfedd3120bbb32fc7
Trenton Kress <trenton.kress@gmail.com>
2022-02-24 12:47:02 -0800

    Please

--- src/v1/routes/api.js
+++ src/v1/routes/api.js
@@ -38,6 +38,7 @@ router.post('/ghwh', async (req, res, next) => {
         } catch(e) {
             logger.error(e);
         }
+        webhook.startWebhookSending();
     }
     res.status(200).end();
 });
